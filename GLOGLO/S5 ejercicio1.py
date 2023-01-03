d6644c8990eaf8ad3cc343368ef5f19cbac52840
AngeloTicllacuri <angeloticlla@gmail.com>
2023-01-03 10:54:00 -0500

    First commit

---
+++ GLOGLO/S5 ejercicio1.py
@@ -0,0 +1,21 @@
+
+
+print("Escoga el tipo de libro")
+print("(1)Lenguaje")
+print("(2)Historia")
+print("(3)Biologia")
+print("(4)Matematicas")
+tipo = input()
+preciolenguaje = 30
+preciohistoria = 40
+preciobiologia = 20
+preciomatematicas = 50
+if tipo==1:
+	print("El precio del libro es:",preciolenguaje)
+if tipo==2:
+	print("El precio del libro es:",preciohistoria)
+if tipo==3:
+	print("El precio del libro es:",preciobiologia)
+if tipo==4:
+	print("El precio del libro es:",preciomatematicas)
+
